ffc4695eaf6cb5d28e3404b761bb9da4fc3cc37c
Mbk <47031403+MBK8@users.noreply.github.com>
2020-05-13 15:09:51 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`,
+        icon: `src/images/crest.png`,
       },
     },
   ],
