f6da04021f2f05c4ce896fc7fec9b92fb2fd29dc
Mbk <47031403+MBK8@users.noreply.github.com>
2020-05-13 14:28:42 +0000

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import styled from "styled-components"
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
-  background-image: url(${require("../images/bg.webp")});
+  background-image: url(${require("../images/bg.jpg")});
   background-position: center;
   background-size: cover;
   display: flex;
